ed984862540ddde605abb3d20a6717447da5293e
Yusef McCaskill <yusef.mccaskill@mrjaffesclass.com>
2022-09-30 14:21:33 -0700

    initial commit

---
+++ IfStatements/ifStatements1.java
@@ -0,0 +1,42 @@
+public class ifStatements1
+{
+
+    int a = 5;
+    int b = 2;
+    int c = 1;
+    int d = -8;
+    
+    public void run() {
+        if (a == 5) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if (b == 2) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if (c != 1) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        
+        if (d == -8 && a == 4) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        System.out.println("HelloWorld!");
+    }
+    
+    public static void main(String[] args)
+    {
+        ifStatements1 hw = new ifStatements1();
+        hw.run();
+    }
+}
+
